7efa0a3fc54170663624df9dd1dd17445d8c601d
phatcm <phat.caominh.dev@gmail.com>
2022-11-30 19:33:30 +0700

    sftp, upload, download

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ def main(dir):
                            3. Press '3' to upload data to sftp.\n
                            4. Press '4' to download data from sftp.\n
                            5. Press '5' to decompress and decrypt.\n
-                           6. Press '6' to create database.\n"""))
+                           6. Press '6' to create database.\n
+                           7. Press '7' to run ETL process"""))
         if choice == 1:
             try:
                 c = Compressor(path)
@@ -30,34 +31,69 @@ def main(dir):
             except:
                 print("Something were wrong when encrypt file!\n")
         elif choice == 3:
-                with pysftp.Connection(host=sftpHost, port=sftpPort, username=uname, private_key=privateKeyFilePath, cnopts=cnopts) as sftp:
-                    print('Connected to sftp server')
-                    sftp.cwd("C:/remFolder")
+            with pysftp.Connection(host=sftpHost, port=sftpPort, username=uname, private_key=privateKeyFilePath, cnopts=cnopts) as sftp:
+                print('Connected to sftp server')
+                sftp.cwd("C:/remFolder")
 
-                    #Only compress is require, encrypt file is optional
-                    try:
-                        sftp.put(zipenc,preserve_mtime=True)
-                        print("Send data.zip.encrypted")
-                        break
-                    except:
-                        sftp.put(zipfile,preserve_mtime=True)
-                        print("Send data.zip")
-                    
+                #Only compress is require, encrypt file is optional
+                try:
+                    sftp.put(zipenc,preserve_mtime=True)
+                    print("Send data.zip.encrypted")
+                    break
+                except:
+                    sftp.put(zipfile,preserve_mtime=True)
+                    print("Send data.zip")
+        elif choice == 4:
+            with pysftp.Connection(host=sftpHost, port=sftpPort, username=uname, private_key=privateKeyFilePath, cnopts=cnopts) as sftp:
+                print('Connected to sftp server')
+                sftp.cwd("C:/remFolder")
+
+                try:
+                    sftp.get(zipenc,preserve_mtime=True)
+                    print("Get data.zip.encrypted")
+                    break
+                except:
+                    sftp.get(zipfile,preserve_mtime=True)
+                    print("Get data.zip")
+        elif choice == 5:
+            try:
+                c = Compressor("./")
+                c.decompress_file('data.zip')
+            except:
+                password = input("Enter password: ")
+                e = Encryptor(password)
+                e.decrypt_file(zipfile)
+                
+                c = Compressor("./")
+                c.decompress_file(zipdec)
+        elif choice == 6:
+            try:
+                database.main()
+                print("Create starschema successfully")
+            except:
+                print("Something were wrong when create starschema")
+        elif choice == 7:
+            print("In progress")
+        
+                
+                
             
 if __name__ == '__main__':
-    
+    #Direction setup
+    current = "./"
     path = "data/"
     zipfile = path[0:-1]+".zip"
     zipenc = zipfile+".encrypted"
     zipdec = zipfile+".decrypted"
-    dir = os.listdir(path)
+    dir = os.listdir(current)
     
+    #Postgresql setup
     sftpHost = 'localhost'
     sftpPort = 22
     uname = 'Admin'
     privateKeyFilePath = './id_rsa'
-
     cnopts = pysftp.CnOpts(knownhosts=r'C:\Users\Admin\.ssh\known_hosts')
+    
     main(dir)
 
 
